4a4f0fe8903832d6a90cc5b38cfc6a1a308baf19
pallavithiruna <114101268+pallavithiruna@users.noreply.github.com>
2022-10-01 18:58:19 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,19 @@
+var player=[
+{name:'raju',age:24,game:'chess'},
+{name:'lalitha',age:30,game:'badmiton'},
+{name:'kishore',age:27,game:'cricket'},
+{name:'pallavi',age:26,game:'volley ball'}
+]
+
+
+//for(var value of player){
+  //  console.log(value.name+" is " +value.age+" age plays "+value.game);
+//}
+
+
+//for(var key in player){
+  //console.log(player[key])
+//}
+
+
+player.forEach((element)=>console.log(element));
