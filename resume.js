d3536c57808c0ee2e2317ac6783d551df7c4099a
pallavithiruna <114101268+pallavithiruna@users.noreply.github.com>
2022-10-01 21:43:07 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,29 @@
+var resume={
+    CareerStatement:"To pursue a challenging career and a part of evolve organisation that gives a scope to enhance my lore and providing exposure to new concepts for the growth of the organisation",
+    PersonalSkills:"Ability to grasp and learn new technology quickly,Flexibility,Adaptibility",
+    TechnicalSkills:"MSOffice",
+    ExtraCurricularActivities:"ClassicalDance-Bharata natiyam",
+    
+    Course:"B.E ElectricalAndElectronicsEngineering",Institution:"UniversityCollegeOfEngineering BIT Campus",score:"7.04",YearOfPassing:"2018",
+    Course:"H.S.C",score:"88.33%",Institution:"SRV Girls Higher Secondary School",YearOfPassing:"2014",
+    Course:"SSLC",score:"92.8%",Institution:"St Antony Matriculation School",YearOfPassing:"2012",
+
+    
+    
+        Name:"Pallavi G",
+        FatherName:"Govindaraju K",
+        MotherName:"Aruljothi G",
+        DateofBirth:"19-02-1996",
+        Gender:"Female",
+        Nationality:"Indian",
+        LanguageKnown:"English,Tamil,Hindi",
+        MaritalStatus:"Unmarried",
+        Address:"373-c,Type-2,Block-29,Neyveli,Cuddalore,Pincode-607807",
+
+
+
+    Declaration:"I hereby declare that above information was true and correct to best of my knowledge and belief"
+    }
+    console.log(JSON.stringify(resume));
+   
+    
