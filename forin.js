45138d3973a9d0b4609ffb85acd1870a8d1273ac
pallavithiruna <114101268+pallavithiruna@users.noreply.github.com>
2022-10-01 19:06:41 +0530

    Add files via upload

---
+++ forin.js
@@ -0,0 +1,12 @@
+var player=[
+{name:'raju',age:24,game:'chess'},
+{name:'lalitha',age:30,game:'badmiton'},
+{name:'kishore',age:27,game:'cricket'},
+{name:'pallavi',age:26,game:'volley ball'}
+]
+
+
+for(var key in player){
+  console.log(player[key])
+}
+
